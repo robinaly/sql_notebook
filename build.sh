30f7f58ab10edb8d448f4725b69858fba4d27f6f
Robin Aly <robin@aly.de>
2016-05-19 08:46:05 +0200

    Minor typos

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# Grabs the newest version of the sql kernel and replaces the git hash in Dockerfile such that the kernel is always up to date
+# Greps the newest version of the sql kernel and replaces the git hash in Dockerfile such that the kernel is always up to date
 # with the git repository.
 #
 ORG=$PWD
@@ -11,4 +11,4 @@ HASH=$(git rev-parse HEAD)
 cd $ORG
 rm -rf $OUT
 sed -i.bak -e "s!RUN pip install git+https://github.com/robinaly/sql_kernel.git@.*!RUN pip install git+https://github.com/robinaly/sql_kernel.git@$HASH!" Dockerfile
-docker build -t "sql-notebook" .
+docker build -t "sql_notebook" .
